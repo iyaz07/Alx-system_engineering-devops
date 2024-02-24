9818d0dd7197ea9d5db9c1cd4d634b14862acf6a
iyaz07 <moseseniade@gmail.com>
2024-02-24 04:44:02 -0800

    Change To Flask Compatible Werkzeug version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Install flask
-package { 'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+# Installing flask
+
+package { 'python3-pip':
+  ensure => 'installed',
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
+}
+
