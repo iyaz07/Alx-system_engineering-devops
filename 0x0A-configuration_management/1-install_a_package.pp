0c42729a87d3519acc54da99f0bdba3e5592ca97
iyaz07 <moseseniade@gmail.com>
2024-02-28 20:58:18 -0800

    Check flask install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 #!/usr/bin/pup
 # Installing flask (2.1.0)
+# Installing flask
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
 }
