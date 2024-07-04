1d28d585545c6ef793cce14bc6c689b20bf3febd
Iyaz <moseseniade@gmail.com>
2024-07-04 08:08:48 -0700

    Update config in system-wide config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,32 +1,12 @@
-file { '/root/.ssh':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0700',
-}
+# Configure the SSH server to authenticates only through SSh keys
 
-# Ensure the SSH configuration file exists
-file { '/root/.ssh/config':
-  ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0600',
-  require => File['/root/.ssh'], # Ensure directory exists before creating the file
+file_line { ' Turn off passwd auth':
+  path     => '/etc/ssh/ssh_config',
+  line     => ' PasswordAuthentication no',
+  replace  => true,
 }
-
-# Add or update the IdentityFile line in the SSH configuration file
 file_line { 'Declare identity file':
-  path  => '/root/.ssh/config',
-  line  => '  IdentityFile ~/.ssh/school',
-  match => '^\\s*IdentityFile\\s+',
-  require => File['/root/.ssh/config'], # Ensure file exists before adding/updating the line
+  path     => '/etc/ssh/ssh_config',
+  line     => ' IdentityFile ~/.ssh/school',
+  replace  => true,
 }
-
-# Add or update the PasswordAuthentication line in the SSH configuration file
-file_line { 'Turn off passwd auth':
-  path  => '/root/.ssh/config',
-  line  => '  PasswordAuthentication no',
-  match => '^\\s*PasswordAuthentication\\s+',
-  require => File['/root/.ssh/config'], # Ensure file exists before adding/updating the line
-}
-
